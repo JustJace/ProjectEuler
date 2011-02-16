d732c554e074a85292b37d30867d018febe7f0c0
JustJace <jacebrewster@gmail.com>
2011-02-16 12:46:34 -0600

    Fixed p010.rb

--- p010.rb
+++ p010.rb
@@ -1,7 +1,6 @@
 # Problem 10 : Find the sum of all the primes below two million.
 ## Runs really slowly, though
 
-$primes
 
 def prime(n)
 	for i in $primes do
@@ -13,7 +12,7 @@ def prime(n)
 	return true
 end
 
-primes = [2]
+$primes = [2]
 t = 2
 
 for i in 3...2000000 do
