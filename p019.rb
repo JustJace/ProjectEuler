8157f43d5527077cedbc2361151ffb7c2a1f11d0
Jace Brewster <jacebrewster@gmail.com>
2012-07-20 15:01:50 -0500

    Solved p019 and p026

--- p019.rb
+++ p019.rb
@@ -1,1 +1,30 @@
 # Problem 19 : How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+def StepDay day
+
+	return (day + 1) % 7
+end
+
+monthdays = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+dayofweek = 1
+
+solutionCount = 0
+
+for year in 1901..2000
+	for month in 0..11
+		days = monthdays[month]
+		if (month == 1 && year % 4 == 0)
+			days += 1
+		end
+		for day in 1..days
+			dayofweek = StepDay dayofweek
+			if day == 1 && dayofweek == 0
+				solutionCount += 1
+			end
+		end
+	end
+end
+
+puts solutionCount
+
--- p026.rb
+++ p026.rb
@@ -1,9 +1,25 @@
 # Find the value of d<1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 
-require 'bigdecimal'
+bestRepeat = 0
+bestD = 0
+for d in 1...1000
+	hash = []
+	r = 1
+	while true
+		hash.push [r,d]
+		r = r % d
+		r = r * 10
 
-best = 0
-for d in 1...4
-	fraction = BigDecimal("1",100).div BigDecimal(d.to_s,100)
-	puts fraction.to_s("F")
+		if hash.include? [r,d]
+			repeat = hash.length - hash.index([r,d])
+			if repeat > bestRepeat
+				bestRepeat = repeat
+				bestD = d
+			end
+
+			break
+		end
+	end
 end
+
+puts bestD
