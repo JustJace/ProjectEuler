22284d92cc30e63ab2a560fbf5b03496cd4ad5ab
Jace Brewster <jacebrewster@gmail.com>
2012-07-27 16:35:51 -0500

    solved p067

--- p067.rb
+++ p067.rb
@@ -1,48 +1,24 @@
 # Problem 67 : Find the maximum total sum from top to bottom of the triangle in triangle.txt
 
-
-# Trim? Returns a boolean whether or not the traverse should trim that node
-
-def trim?(depth, index, psum)
-
-	return false if depth < 20 #$tri.length / 2
-	return true  if (depth.to_f / $tri.length.to_f) * $sum * 0.9 > psum
-
+def Max a,b
+	return a > b ? a : b
 end
 
-
-# Traverse at depth, index, and partial sum
-def traverse(depth, index, psum)
-
-	# Add the Node
-	psum += $tri[depth][index].to_i
-
-	# trim?
-	return if trim?(depth, index, psum)
-
-	# If max depth is reached
-	if depth == $tri.length - 1
-		# And the total is greater
-		if psum > $sum
-			$sum = psum
-			puts $sum
-		end
-	else
-		# Traverse Left
-		traverse(depth + 1, index, psum)
-		# Traverse Right
-		traverse(depth + 1, index + 1, psum)
-	end
-
-	return
-end
 
 # Main
-$sum = 0
-$tri = []
+
+tri = []
 f = File.open("triangle.txt", "r")
 while line = f.gets
-	$tri.push line.split(' ')
+	tri.push line.split(' ').map{|t| t.to_i}
 end
-traverse(0,0,0)
-puts $sum
+
+for depth in (tri.length - 2).downto(0)
+	for index in 0...tri[depth].length
+		a = tri[depth + 1][index]
+		b = tri[depth + 1][index + 1]
+		tri[depth][index] += Max(a, b)
+	end
+end
+
+puts tri[0][0]
